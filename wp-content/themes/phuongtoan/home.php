826cf42193a0dadcb28c838f3a0827d5656f6fc2
An <an.ho1991@gmail.com>
2015-11-05 22:04:03 +0700

    Update Homepage

--- wp-content/themes/phuongtoan/home.php
+++ wp-content/themes/phuongtoan/home.php
@@ -7,9 +7,11 @@
 get_header();
 
 echo do_shortcode("[huge_it_slider id='1']"); //Display Homepage Slider
-
 ?>
 
+<div class="home-page-content">
+	<?php echo get_page(14)->post_content; ?>
+</div>
 	
 
 <?php get_footer(); ?>
