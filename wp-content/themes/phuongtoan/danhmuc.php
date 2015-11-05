ed8861f1d78a6eee7d2913ad2921056e11d780d5
githubminh <minh@wiserobot.com>
2015-11-06 00:11:24 +0700

    update

--- wp-content/themes/phuongtoan/danhmuc.php
+++ wp-content/themes/phuongtoan/danhmuc.php
@@ -7,7 +7,13 @@
 get_header();
 ?>
 
-<h1>Danh muc</h1>
-	
+<h1>Danh Mục Sản Phẩm</h1>
+<?php
+	$category_ids = get_all_category_ids();
+	foreach($category_ids as $cat_id) {
+		$cat_name = get_cat_name($cat_id);
+		echo $cat_name.'<br>';
+	}
+?>
 
 <?php get_footer(); ?>
