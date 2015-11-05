351ec552a5d9496c2feb6ab38773feea7c115fdb
An <an.ho1991@gmail.com>
2015-11-05 23:37:01 +0700

    Update Home Content

--- wp-content/themes/phuongtoan/home.php
+++ wp-content/themes/phuongtoan/home.php
@@ -9,8 +9,13 @@ get_header();
 echo do_shortcode("[huge_it_slider id='1']"); //Display Homepage Slider
 ?>
 
-<div class="home-page-content">
-	<?php echo get_page(14)->post_content; ?>
+<div id="home-page-content">
+	<h2>Phương Toàn</h2>
+	<ul>
+		<li><span></span>Chuyên tư vấn - thiết kế - thi công: led, alu, pano, chữ nổi, bảng hiệu, hộp đèn, cửa, cầu thang, lan can, nhà tiền chế, mái xếp lượn sóng, mái tôn, nhà vòm, mái hiên di động, tấm lợp lấy sáng poly...</li>
+		<li><span></span>Thiết kế - thi công cafe sân vườn, quán ăn, nhà hàng, trường học, hồ bơi...</li>
+		<li><span></span>Nhận thi công tất cả các tỉnh &amp; thành phố</li>
+	</ul>
 </div>
 	
 
