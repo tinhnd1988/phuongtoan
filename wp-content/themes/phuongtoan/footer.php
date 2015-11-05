7eb2b6b922cb94b22e590f416c26f50d730a2452
githubminh <minh@wiserobot.com>
2015-11-05 21:08:31 +0700

    update

--- wp-content/themes/phuongtoan/footer.php
+++ wp-content/themes/phuongtoan/footer.php
@@ -12,11 +12,33 @@
 
 	<div id="Footer"> 
 		<div class="content_f"> 
-		Công ty tnhh sản xuất thương mại quảng cáo phương toàn
-		Đc: 110A Lê Niệm, P. Phú Thạnh , Q. Tân Phú 
-ĐT: 08 3606 2577 / 0905 333 188
-Fax: 08 3978 3442 
-Email: phuongtoan.vn@gmail.com
+			<div class="footer_left">
+				<ul class="menu_footer">
+					<li> <a href="/">Trang chủ</a> </li>
+					<li> <a href="/">Giới thiệu</a> </li>
+					<li> <a href="/">Danh mục sản phẩm</a> </li>
+					<li> <a href="/">Công trình tiêu biểu</a> </li>
+					<li> <a href="/">Liên hệ</a> </li>
+				</ul>
+				<div><span class"icon hom"></span>Công ty tnhh sản xuất thương mại quảng cáo phương toàn
+				Đc: 110A Lê Niệm, P. Phú Thạnh , Q. Tân Phú </div>
+				<div class=""> 
+					<div><span class="icon call"></span> ĐT: 08 3606 2577 / 0905 333 188</div>
+					<div><span class="icon fax"></span> Fax: 08 3978 3442 </div>
+				</div>
+				<div><span class="icon email"></span>Email: phuongtoan.vn@gmail.com </div>
+			</div>
+			<div class="footer_right">
+				<h3 class="title">STAY CONNECTED</h3>
+				<ul>
+					<li><a href="#" class="icon face"></a> </li>
+					<li><a href="#" class="icon twitter"></a> </li>
+					<li><a href="#" class="icon google"></a> </li>
+					<li><a href="#" class="icon youtube"></a> </li>
+					<li><a href="#" class="icon printer"></a> </li>
+				</ul>
+				<span>LIKE US on FACEBOOK</span>
+			</div>
 	</div>
 <div class="footer_bottom"> 
 <div class="bg_top"></div>
