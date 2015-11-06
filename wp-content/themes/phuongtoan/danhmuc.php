33270ac287434a57398e46bd2181acc8e693f3b3
githubminh <minh@wiserobot.com>
2015-11-06 09:23:00 +0700

    update

--- wp-content/themes/phuongtoan/danhmuc.php
+++ wp-content/themes/phuongtoan/danhmuc.php
@@ -6,30 +6,30 @@
 */
 get_header();
 ?>
+<div class="DanhMuc">
+	<h1>Danh Mục Sản Phẩm</h1>
+	<ul>
+	<?php
+	$args = array(
+		'type'                     => 'post',
+		'child_of'                 => 0,
+		'parent'                   => 0,
+		'orderby'                  => 'name',
+		'order'                    => 'ASC',
+		'hide_empty'               => 1,
+		'hierarchical'             => 1,
+		'exclude'                  => '',
+		'include'                  => '',
+		'number'                   => '',
+		'hide_empty'               => 0,
+		'taxonomy'                 => 'danh-muc',
+		'pad_counts'               => false 
 
-<h1>Danh Mục Sản Phẩm</h1>
-<ul>
-<?php
-$args = array(
-	'type'                     => 'post',
-	'child_of'                 => 0,
-	'parent'                   => 0,
-	'orderby'                  => 'name',
-	'order'                    => 'ASC',
-	'hide_empty'               => 1,
-	'hierarchical'             => 1,
-	'exclude'                  => '',
-	'include'                  => '',
-	'number'                   => '',
-	'hide_empty'               => 0,
-	'taxonomy'                 => 'danh-muc',
-	'pad_counts'               => false 
-
-); 
-	$categories = get_categories($args);
-	foreach($categories as $category) : ?>
-		<li><a href="<?php echo get_term_link( $category->slug, 'danh-muc' );?>"> <?php echo $category->name; ?></a>  </li>		
-	<?php endforeach; ?>
-</ul>
-
+	); 
+		$categories = get_categories($args);
+		foreach($categories as $category) : ?>
+			<li><a href="<?php echo get_term_link( $category->slug, 'danh-muc' );?>"> <?php echo $category->name; ?></a>  </li>		
+		<?php endforeach; ?>
+	</ul>
+</div>
 <?php get_footer(); ?>
