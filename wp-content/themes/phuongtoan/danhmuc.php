7f19ed2c9fb84b9a2d7f804fdf32c0e945eec089
githubminh <minh@wiserobot.com>
2015-11-10 23:02:34 +0700

    update

--- wp-content/themes/phuongtoan/danhmuc.php
+++ wp-content/themes/phuongtoan/danhmuc.php
@@ -53,23 +53,27 @@ get_header();
 			    'order' => 'ASC'
 			));?>
 			<div class="display_none <?php if($j==1) echo 'show_div';?>" id="tabs-<?php echo $category->term_id;?>">
+				<ul>
 		    	<?php foreach( $products as $product ) :
 			    	$meta_value_cate = get_post_meta($product->ID); 
 			    	$url_cate = wp_get_attachment_url( get_post_thumbnail_id($product->ID) );
 			        $img_featured = get_the_post_thumbnail($product->ID, 'medium');
 			        $permalink = get_permalink($product->ID);
 		    		?>
-					<div class="box">
-						<a href="<?php echo $permalink; ?>">
-							<h2><?php echo $product->post_title; ?></h2>
-							<?php if ($img_featured): ?>
-								<div class="imgsp"><?php echo $img_featured; ?></div>
-								<?php else: ?>
-									<img src="<?php bloginfo('template_url');?>/images/noimages.jpeg" width="220" height="190">
-							<?php endif; ?>					
-						</a>
-					</div>
+		    		<li>
+						<div class="box">
+							<a href="<?php echo $permalink; ?>">
+								<h2><?php echo $product->post_title; ?></h2>
+								<?php if ($img_featured): ?>
+									<div class="imgsp"><?php echo $img_featured; ?></div>
+									<?php else: ?>
+										<img src="<?php bloginfo('template_url');?>/images/noimages.jpeg" width="220" height="190">
+								<?php endif; ?>					
+							</a>
+						</div>
+					</li>
 		    	<?php $j++; endforeach;?>
+		    	</ul>
 		    </div>
 		<?php endforeach;?>
 	</div>
