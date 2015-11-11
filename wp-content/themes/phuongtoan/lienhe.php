ab91bbbc49f4896fc88923e50d38f8c4b2636376
githubminh <minh@wiserobot.com>
2015-11-11 16:10:05 +0700

    update

---
+++ wp-content/themes/phuongtoan/lienhe.php
@@ -0,0 +1,25 @@
+<?php
+/*
+/*
+/* Template Name: Lien He
+/*
+*/
+get_header();
+?>
+
+<div id="lienhe-page-content">
+	<div class="Left_page">
+		<div class="lien_he_page">
+			<span class="name_cty">Công ty tnhh sản xuất thương mại quảng cáo phương toàn</span><br>
+			<span>ĐT: 08 3606 2577 / 0905 333 188</span><br>
+			<span>Email: phuongtoan.vn@gmail.com</span>
+		</div>
+		<?php get_template_part( 'content', 'page' ); ?>
+	</div>
+	<div class="right_page">
+		map
+	</div>
+</div>	
+	
+
+<?php get_footer(); ?>
