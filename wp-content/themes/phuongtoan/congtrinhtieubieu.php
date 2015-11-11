1ab59aec9905799407084aaafd2cb0fa4eddc0e8
githubminh <minh@wiserobot.com>
2015-11-11 21:37:29 +0700

    update

--- wp-content/themes/phuongtoan/congtrinhtieubieu.php
+++ wp-content/themes/phuongtoan/congtrinhtieubieu.php
@@ -8,8 +8,34 @@ get_header();
 ?>
 
 <div id="CTTB-page-content">
-	
+
 	<ul>
+		<?php $products = query_posts(array( 
+			    'post_type' => 'post',
+			    'showposts' => -1,
+			    'tax_query' => array(
+			    	array(
+				        'taxonomy' => 'category',
+				        'terms' => 14,
+				        'field' => 'term_id'
+				    ),
+			    ),
+			    'orderby' => 'title',
+			    'order' => 'ASC'
+			)); 
+		foreach( $products as $product ) :
+			    	$meta_value_cate = get_post_meta($product->ID); 
+			    	$url_cate = wp_get_attachment_url( get_post_thumbnail_id($product->ID) );
+			        $img_featured = get_the_post_thumbnail($product->ID, 'medium');
+			        $permalink = get_permalink($product->ID); ?>
+			        <li>
+			        	<div>
+			        		<?php print_r($product); ?>
+			        		<h2><?php echo $product->post_title; ?></h2>
+			        	</div>
+			        </li>
+		<?php endforeach;
+		?>
 		<li></li>
 	</ul>
 </div>	
