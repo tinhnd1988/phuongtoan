7befbfdbc8729653fdb4e6ee7c1c350cfe6a68bf
githubminh <minh@wiserobot.com>
2015-11-11 22:21:21 +0700

    update

--- wp-content/themes/phuongtoan/lienhe.php
+++ wp-content/themes/phuongtoan/lienhe.php
@@ -15,7 +15,7 @@ get_header();
 				<span>ĐT: 08 3606 2577 / 0905 333 188</span><br>
 				<span>Email: phuongtoan.vn@gmail.com</span>
 			</div>
-			<?php get_template_part( 'content', 'page' ); ?>
+			<?php get_page(23); ?>
 		</div>
 		<div class="right_page">
 			map
